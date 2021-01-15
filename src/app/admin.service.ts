0708eac3b2e1375a3b2afa7e96513d35a78f7cf0
rajat26oct <62444798+rajat26oct@users.noreply.github.com>
2021-01-15 22:11:32 +0530

    Admin side login signup works

---
+++ src/app/admin.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot
+} from '@angular/router'
+import {Router} from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService implements CanActivate {
+  url = 'http://localhost:8080/admin';
+  //create an httpclient object
+  constructor(private httpclient: HttpClient, private Router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    //user is already logedin
+    //if token is present user is logedin
+    //launch the component
+    if (sessionStorage['token']) {
+      return true;
+    } else {
+      //stop launching
+      this.Router.navigate(['/admin-signin']);
+      return false;
+    }
+  }
+
+  login(email: string, password: string) {
+    const body = {
+      email: email,
+      password: password,
+    };
+    return this.httpclient.post(this.url + '/login', body);
+  }
+  signup(name: string, email: string, password: string) {
+    const body = {
+      name: name,
+      email: email,
+      password: password,
+    };
+  }
+}
